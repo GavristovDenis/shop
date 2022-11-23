5b1cab3438e0eadb6fe1cf4e3fc23e7023177fc9
Alexandr Prokhorov <58104415+Slameich-web@users.noreply.github.com>
2022-11-23 14:04:07 +0300

    Update Goods.js

--- src/Goods/Goods.js
+++ src/Goods/Goods.js
@@ -6,11 +6,12 @@ import { Modal } from "./Modal"
 export const Goods = () => {
 
     const [filteredData] = useState(CardInfo)
+    {/*TODO filteredData2 серьезно? */}
     const [filteredData2, setFilteredData2] = useState(CardInfo)
     const [lesserNumber, setLesserNumber] = useState(0)
     const [biggerNumber, setBiggerNumber] = useState(0)
 
-
+{/*TODO Вынести*/}
     const AddToCart = (name, price) => {
         if (cart.length === 10) {
             return
@@ -25,12 +26,14 @@ export const Goods = () => {
             setCart(addedCart)
         }
     }
+    {/*TODO Вынести*/}
     const FilterData = () => {
         setFilteredData2(filteredData.filter(v => parseInt(v.price) >= lesserNumber && parseInt(v.price) <= biggerNumber))
         if (!lesserNumber && !biggerNumber) {
             return setFilteredData2(filteredData)
         }
     }
+    {/*TODO Вынести*/}
     const Close = () => {
 
         setModalOn(false)
@@ -40,12 +43,16 @@ export const Goods = () => {
     const [modalOn, setModalOn] = useState(false)
     return (
         <div>
+        {/*TODO Вынести*/}
             {modalOn ? <Modal Close={Close} cart={cart} /> : ""}
             <Filter FilterData={FilterData} setLesserNumber={setLesserNumber} setBiggerNumber={setBiggerNumber} />
+                {/*TODO нормальный alt*/}
             <div className="cart_button"><img src="https://catherineasquithgallery.com/uploads/posts/2021-02/1612606907_117-p-telezhka-na-zelenom-fone-209.png" alt="123" className="cart_image" onClick={() => { setModalOn(true) }}></img><span className="cart_number">{cart.length}</span></div>
             <div className="goods">
                 <div className="cards">
+                    {/*TODO Убрать return*/}
                     {filteredData2.map((item, index) => {
+                        {/*TODO деструктуризация*/}
                         return (
                             <GoodsCard key={index} name={item.name} price={item.price} img={item.img} AddToCart={AddToCart} />
                         )
