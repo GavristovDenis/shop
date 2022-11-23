668f6aeca62de7c8b9755de803589b970bd0e1be
Alexandr Prokhorov <58104415+Slameich-web@users.noreply.github.com>
2022-11-23 13:58:27 +0300

    Update MainPage.js

--- src/Info/MainPage.js
+++ src/Info/MainPage.js
@@ -6,14 +6,17 @@ import { Info } from "./MockData";
 export function MainPage() {
 
     return (
+        {/*TODO Вынести*/}
         <div>
             <div className="main_page">
                 <div className="main_page_text">
                     Добро пожаловать на ВсяМебель.ru
                 </div>
             </div>
+        {/*TODO Вынести*/}
             <div className="info_bracket_container_body">
                 <div className="info_bracket_container">
+                    {/*TODO Убрать return*/}
                     {Info.map((item, index) => {
                         return (
                             <InfoBracket key={index} img={item.img} text={item.text} />
